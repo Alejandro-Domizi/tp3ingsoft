355d23d4dce26d0be20ff0bae17132ca679d12ba
alejandro-domizi <alejandrodomizi@gmail.com>
2021-09-09 02:56:11 -0300

    el programa debera convertir pesos argentinos en dolares, segun valor de conversion

---
+++ conversor.php
@@ -0,0 +1,21 @@
+<!-- Subir un repositorio git a Github con un programa sencillo en PHP (cada estudiante debe inventar una consigna sencilla, un programa que tenga entre 10 y 50 líneas de código aproximadamente).
+
+Dicho repositorio debe tener al menos 3 commits -->
+
+<!-- Primer commit: La “consigna” (el objetivo) del programa escrita como comentario. -->
+
+<!-- Segundo commit: El programa sin concluir, “a mitad de camino”.  -->
+
+<!-- Commits subsiguientes: Todos los commits intermedios que hagan falta. -->
+
+<!-- Último commit: El programa terminado -->
+
+<?php
+
+/*se propone que el programa a partir del ingreso del valor de conversion, 
+(que seria el valor en dolares de un 1 peso argentino en el momento),
+luego se pedira en el recudro (Pesos) el valor de la cantidad numerica de pesos argentinos
+que se requiera convertir a dolares, el programa dira cuanto es el equivalente en dolares
+rellenando el cuadro (Dolares). 
+*/
+
